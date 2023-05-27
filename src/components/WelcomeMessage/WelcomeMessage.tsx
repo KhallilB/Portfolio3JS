59ac407dba0ebd6edb0fda5211cbdd09bba761f8
KhallilB <khallil.bailey@presidioidentity.com>
2023-05-27 14:20:33 -0700

    add welcome message

---
+++ src/components/WelcomeMessage/WelcomeMessage.tsx
@@ -0,0 +1,10 @@
+const WelcomeMessage = () => {
+    return (
+        <div>
+            <h1>Hi, I'm Kallil</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut malesuada nibh. Nam condimentum, neque ornare venenatis scelerisque, magna libero pulvinar risus, quis maximus libero lectus non lacus. Sed nisl nulla, scelerisque nec aliquet at, pharetra ut mauris. Maecenas faucibus magna accumsan, efficitur ligula eu, semper tortor. Nam ac sagittis erat. Quisque auctor nulla ut odio aliquam, a tempor nibh ornare. Integer aliquet dolor risus, sit amet suscipit dolor condimentum ut. Vestibulum mi nibh, lobortis et velit vel, varius accumsan lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus id nibh non urna ullamcorper pretium. Mauris ac commodo mauris, sit amet egestas orci. Mauris vitae velit ac dui maximus pretium eget quis purus. Quisque vehicula urna tortor, et pellentesque nunc ultricies eu. Cras tempus sem neque, sit amet luctus risus condimentum sit amet. Curabitur suscipit, purus sit amet consequat porttitor, sem risus vehicula est, non molestie risus arcu eu mi.</p>
+        </div>
+    )
+}
+
+export default WelcomeMessage;
