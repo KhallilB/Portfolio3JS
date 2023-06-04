01dba4ccf5bcf3187b1472d7465142b556f0385f
KhallilB <khallil.bailey@presidioidentity.com>
2023-06-03 23:47:03 -0700

    move website

--- src/components/appCard/appInfo/appInfo.tsx
+++ src/components/appCard/appInfo/appInfo.tsx
@@ -6,11 +6,11 @@ const AppInfo = (): JSX.Element => {
   return (
     <div className="app_info_container">
       <h3>Title</h3>
+      <p>Website</p>
       <div className="screenshots">
         <h4>Screenshots</h4>
         <ScreenshotCarousel />
       </div>
-      <p>Website</p>
       <p>Tech Stack</p>
       <p>App Summary</p>
     </div>
