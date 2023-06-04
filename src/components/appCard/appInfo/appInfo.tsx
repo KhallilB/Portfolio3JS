53b7fb0730fc199c7b8c0e016fc1a3d6e09e0c85
KhallilB <khallil.bailey@presidioidentity.com>
2023-06-03 23:46:47 -0700

    screenshot carousel

--- src/components/appCard/appInfo/appInfo.tsx
+++ src/components/appCard/appInfo/appInfo.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import ScreenshotCarousel from "../screenshotCarousel/screenshotCarousel";
+
 const AppInfo = (): JSX.Element => {
   return (
     <div className="app_info_container">
       <h3>Title</h3>
+      <div className="screenshots">
+        <h4>Screenshots</h4>
+        <ScreenshotCarousel />
+      </div>
       <p>Website</p>
       <p>Tech Stack</p>
       <p>App Summary</p>
---
+++ src/components/appCard/screenshotCarousel/screenshotCarousel.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { Image, Row } from "@nextui-org/react";
+
+const ScreenshotCarousel = (): JSX.Element => {
+  return (
+    <Row gap={1} justify="center">
+      <Image
+        width={300}
+        height={180}
+        src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+        alt="Default Image"
+        objectFit="cover"
+      />
+      <Image
+        width={300}
+        height={180}
+        src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+        alt="Default Image"
+        objectFit="cover"
+      />
+      <Image
+        width={300}
+        height={180}
+        src="https://github.com/nextui-org/nextui/blob/next/apps/docs/public/nextui-banner.jpeg?raw=true"
+        alt="Default Image"
+        objectFit="cover"
+      />
+    </Row>
+  );
+};
+
+export default ScreenshotCarousel;
