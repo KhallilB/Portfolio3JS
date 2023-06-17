f327492b077a83a95a436cd976ab77671d9f1aa7
KhallilB <khallil.bailey@presidioidentity.com>
2023-06-17 03:19:34 -0700

    headshot

--- src/sections/Welcome/Welcome.tsx
+++ src/sections/Welcome/Welcome.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { Col, Container } from "@nextui-org/react";
+import { useTheme, Col, Row, Container, Text, Image } from "@nextui-org/react";
+
+import headshot from "../../assets/headshot/Khallil_Bailey.png";
 
 const Welcome = (): JSX.Element => {
+  // const [headshotHeight, setHeadshotHeight] = useState("25em");
+
+  const { theme } = useTheme();
+  // const { width } = useWindowDimensions();
+
   return (
     <Container responsive>
-      <Col>
-        <h1>Hi,{"I'm"} Khallil</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut
-          malesuada nibh. Nam condimentum, neque ornare venenatis scelerisque,
-          magna libero pulvinar risus, quis maximus libero lectus non lacus. Sed
-          nisl nulla, scelerisque nec aliquet at, pharetra ut mauris. Maecenas
-          faucibus magna accumsan, efficitur ligula eu, semper tortor. Nam ac
-          sagittis erat. Quisque auctor nulla ut odio aliquam, a tempor nibh
-          ornare. Integer aliquet dolor risus, sit amet suscipit dolor
-          condimentum ut. Vestibulum mi nibh, lobortis et velit vel, varius
-          accumsan lacus. Interdum et malesuada fames ac ante ipsum primis in
-          faucibus. Vivamus id nibh non urna ullamcorper pretium. Mauris ac
-          commodo mauris, sit amet egestas orci.
-        </p>
+      <Col span={5}>
+        <Row align="center" css={{ border: "solid 1px orange" }}>
+          <Text h1 em css={{ fontSize: theme?.fontSizes["5xl"].value }}>
+            Hi, {" I'm"} Khallil
+          </Text>
+        </Row>
+        <Row
+          align="center"
+          css={{ border: "solid 1px orange", position: "absolute" }}
+        >
+          <Image height={100} src={headshot} />
+        </Row>
+      </Col>
+
+      <Col span={5}>
+        <Text css={{ fontSize: theme?.fontSizes.xl.value }}>
+          As an experienced and driven software developer residing in San
+          Francisco, CA, I am confident in my ability to bring valuable skills
+          and contributions to any technology-focused organization. With an
+          extensive background in programming and a strong desire to continually
+          learn and improve, I am eager to tackle new challenges and make
+          impactful contributions to innovative projects.
+        </Text>
       </Col>
     </Container>
   );
